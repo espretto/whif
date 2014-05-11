a95f6e8d473d940d124eb9de9a7fdc2d6ac1f9f0
Marius Runge <espretto@gmail.com>
2014-05-11 21:47:49 +0200

    devdeps and docs

--- src/promise.js
+++ src/promise.js
@@ -43,6 +43,13 @@
 
   function Promise( then ) {
 
+    // constructor:
+    // 
+    // - allow to omit the `new` operator
+    // - keep private `_state` information
+    // - keep registered call-/errbacks within `_queue`
+    // - pass this' `fulfill` and `reject` functions to the optional intial `then`
+
     var that = this;
 
     if(!(that instanceof Promise)) return new Promise( then );
@@ -66,6 +73,12 @@
 
     then: function( onFulfilled, onRejected ) {
 
+      // __then:__
+      // 
+      // - create a new promise as required to be returned
+      // - enqueue the triple
+      // - `_run()` in case this promise was already fulfilled/rejected
+
       var that = this,
         promise = new Promise();
 
@@ -81,20 +94,31 @@
     },
 
     fulfill: function( value ) {
+
+      // __fulfill:__
+      // provide alternative to initial `then` method
       this._resolve( value );
     },
 
     reject: function( reason ) {
+
+      // __reject:__
+      // provide alternative to initial `then` method
       this._transition( REJECTED, reason );
     },
 
     _transition: function( state, value ) {
 
+      // private __transition__:
+      // 
+      // - transition this promise from one state to another
+      //   and take appropriate actions - delegate to `_run()`
+      // - allow fulfill/reject without value/reason
+      // - be confident `state` will always be one of the defined
+
       var that = this,
         _state = that._state;
 
-      // - allow fulfill/reject without value/reason
-      // - be confident `state` will always be one of the defined
       if ( _state !== state && _state === PENDING ) {
         that._state = state;
         that._value = value;
@@ -103,6 +127,15 @@
     },
 
     _run: function() {
+
+      // private __run__:
+      // 
+      // - if still `PENDING` return
+      // - flush callstack and await next tick
+      // - dequeue triples in the order registered, for each:
+      //   - call registered fulfill/reject handlers dependent on the transition
+      //   - reject immediately if an erro is thrown
+      //   - `._resolve()` the returned value
 
       var that = this;
 
@@ -136,6 +169,16 @@
     },
 
     _resolve: function( x ) {
+
+      // private __resolve__:
+      // 
+      // - if this is to be resolved with itself - throw
+      // - if `x` is another one of ours adopt its `_state`
+      //   is no longer `PENDING` or else prolong state adoption with `.then()`.
+      // - if `x` is neither none nor primitive and is
+      //   _thenable_ i.e. has a `.then()` method assume it's a promise.
+      //   register this promise as `x`'s successor.
+      // - fulfill/reject this promise with `x` any otherwise
 
       var that = this;
 
