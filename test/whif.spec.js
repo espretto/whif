71114090612199b1236d75e920cd454b5fe41117
espretto <espretto@posteo.net>
2020-04-04 22:09:50 +0200

    [upd] update dev deps

---
+++ test/whif.spec.js
@@ -0,0 +1,33 @@
+
+var whif = require('../src/whif.js');
+
+var adapter = {
+
+  resolved: function (value) {
+    return new whif()._resolve(value);
+  },
+
+  rejected: function (reason) {
+    return new whif()._reject(reason);
+  },
+
+  deferred: function() {
+    var promise = new whif();
+    
+    return {
+      promise: promise,
+
+      resolve: function (value) {
+        promise._resolve(value)
+      },
+      
+      reject: function (value) {
+        promise._reject(value)
+      }
+    };
+  }
+}
+
+describe("Promises/A+ Tests", function () {
+  require("promises-aplus-tests").mocha(adapter);
+});
